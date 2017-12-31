71d8baab11353eb1dc2cb9d01f8659ce5bb1f1f2
maoziliang <mozily@qq.com>
2017-12-31 17:33:57 +0800

    chore: Add module's type definition file

---
+++ src/index.d.ts
@@ -0,0 +1,30 @@
+import CDP = require('chrome-remote-interface');
+
+export = PDFExport;
+
+interface IPDFExportOptions extends CDP.ICDPOptions {
+  chromeBin: string,
+  timeout?: number,
+}
+
+declare class PDFExport {
+  constructor(options: IPDFExportOptions);
+
+  /**
+   * 导出url指向的网页
+   *
+   * @param {string} url
+   * @returns {Promise<Buffer>}
+   * @memberof PDFExport
+   */
+  export(opts: {
+    url: string,
+    cookies?: CDP.ICookieItem[],
+    pdfOptions?: CDP.IPrintToPDFOptions,
+  }): Promise<Buffer>;
+
+  /**
+   * 清理资源
+   */
+  dispose(): Promise<void>;
+}
